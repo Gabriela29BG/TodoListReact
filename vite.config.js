76e5a1d3176647cd7d8da27be40fc281408381db
gaby <gabiela.baltazares.garcia@gmail.com>
2024-01-06 21:36:53 -0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: 'https://Gabriela29BG.github.io/TodoListReact'
-  // Establece las extensiones de archivo que Vite debería reconocer para JSX o TSX
-  extensions: ['.js', '.ts', '.jsx', '.tsx'],
+  
 })
